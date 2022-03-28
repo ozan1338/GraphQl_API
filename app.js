22bd31c65266040a33b1b51903b3b58b6d69c2b8
ozan1338 <akhmadfauzan1338@gmail.com>
2022-03-28 14:37:59 +0700

    Connect to MONGO DB

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require('graphql')
+const mongoose = require("mongoose")
+
+const Event = require('./models/event')
 
 
 const app = express()
@@ -39,25 +42,45 @@ app.use('/graphql', graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return events
-        },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date().toISOString()
+        events: async () => {
+            try {
+                const result = await Event.find()
+                return result.map(event => {
+                    return {...event._doc}
+                })
+            } catch (error) {
+                console.log(error)
+                throw error
             }
-
-            events.push(event)
-            return event
+            
+        },
+        createEvent: async(args) => {
+            try {
+                const event = new Event({
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: args.eventInput.price,
+                    date: new Date().toISOString()
+                })
+                const result = await event.save();
+                return {...result._doc}
+            } catch (error) {
+                console.log(error)
+                throw err;
+            }
+            
         }
     },
     graphiql: true
 }))
 
-app.listen(3001, ()=>{
-    console.log("Server listening on port 3001")
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.d2tbv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3001, ()=>{
+        console.log("Server listening on port 3001")
+    })
 })
+.catch(err => {
+    console.log(err)
+})
+
